264027c635c882d6ac06dc11fa31a50e84014b6a
SANJIKS <sanzarmaratov588@gmail.com>
2023-06-15 19:24:27 +0600

    added comments

--- apps/qsystem/models.py
+++ apps/qsystem/models.py
@@ -5,30 +5,29 @@ from slugify import slugify
 User = get_user_model()
 
 class Queue(models.Model):
-    name = models.CharField(max_length=100)
-    window_number = models.PositiveSmallIntegerField()
-    average_waiting_time = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=100)  # Поле для хранения имени очереди
+    window_number = models.PositiveSmallIntegerField()  # Поле для хранения номера окна
+    average_waiting_time = models.PositiveIntegerField(default=0)  # Поле для хранения среднего времени ожидания
 
     def __str__(self):
-        return self.name
+        return self.name  
     
     class Meta:
-        verbose_name = 'Очередь'
-        verbose_name_plural = 'Очереди'
+        verbose_name = 'Очередь' 
+        verbose_name_plural = 'Очереди' 
 
 class Customer(models.Model):
-    queue = models.ForeignKey(Queue, on_delete=models.CASCADE)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='articles')
-    ticket_number = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_served = models.BooleanField(default=False)
-    served_at = models.DateTimeField(null=True, blank=True)
+    queue = models.ForeignKey(Queue, on_delete=models.CASCADE)  # Внешний ключ для связи с моделью очереди
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')  # Внешний ключ для связи с моделью пользователей
+    ticket_number = models.PositiveIntegerField()  # Поле для хранения номера талона
+    created_at = models.DateTimeField(auto_now_add=True)  # Поле для хранения даты и времени создания
+    is_served = models.BooleanField(default=False)  # Поле для хранения статуса обслуживания
+    served_at = models.DateTimeField(null=True, blank=True)  # Поле для хранения даты и времени обслуживания
 
     def __str__(self):
-        return f"Customer {self.user.username} - Queue: {self.queue}"
+        return f"Customer {self.user.username} - Queue: {self.queue}" 
     
     class Meta:
-        verbose_name = 'Талон'
-        verbose_name_plural = 'Талоны'
-        ordering = ['ticket_number']
+        verbose_name = 'Талон'  
+        verbose_name_plural = 'Талоны'  
+        ordering = ['ticket_number']  # Сортировка объектов по номеру талона
--- apps/qsystem/views.py
+++ apps/qsystem/views.py
@@ -25,84 +25,86 @@ class QueueViewSet(viewsets.ModelViewSet):
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all() 
+    serializer_class = CustomerSerializer  
 
     def get_permissions(self):
-        if self.action == 'create':
-            return [permissions.IsAuthenticated()]
-        return super().get_permissions()
-    
+        if self.action == 'create': 
+            return [permissions.IsAuthenticated()]  
+        return super().get_permissions()  
+
     def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context.update({'request': self.request})
+        context = super().get_serializer_context() 
+        context.update({'request': self.request})  
         return context
-    
 
     @action(detail=True, methods=['post'])
     def mark_as_served(self, request, pk=None):
-        customer = self.get_object()
-        customer.is_served = True
-        customer.served_at = timezone.now()  
-        customer.save()
+        customer = self.get_object()  # Получение объекта Customer по его идентификатору 
+        customer.is_served = True  # Установка is_served в значение True
+        customer.served_at = timezone.now()  # Установка времени обслуживания на текущее время
+        customer.save()  # Сохранение изменений 
 
-        queue = customer.queue
-        served_customers = Customer.objects.filter(queue=queue, is_served=True)
-        total_served_customers = served_customers.count()
-        total_waiting_time = sum((customer.served_at - customer.created_at).seconds // 60 for customer in served_customers)
-        average_waiting_time = total_waiting_time // total_served_customers if total_served_customers > 0 else 0
+        queue = customer.queue  # Получение объекта очереди
+        served_customers = Customer.objects.filter(queue=queue, is_served=True)  # Получение всех обслуженных талонов для данной очереди
+        total_served_customers = served_customers.count()  # Получение общего количества обслуженных талонов
+        
+        total_waiting_time = sum(
+            (customer.served_at - customer.created_at).seconds // 60 for customer in served_customers
+        )  # Вычисление общего времени ожидания для обслуженных талонов (в минутах)
+        average_waiting_time = (
+            total_waiting_time // total_served_customers if total_served_customers > 0 else 0
+        )  # Вычисление среднего времени ожидания (в минутах) или установка в 0, если нет обслуженных талонов
 
-        queue.average_waiting_time = average_waiting_time
-        queue.save()
+        queue.average_waiting_time = average_waiting_time  # Установка среднего времени ожидания для очереди
+        queue.save()  # Сохранение изменений
 
-        return Response({'message': 'Талон обслужен и среднее время обновлено'})
+        return Response({'message': 'Талон обслужен и среднее время обновлено'})  
+
     
 
 
 
 class PrintTicket(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
-        current_time = datetime.now().time()
-        start_time = time(9, 0) 
-        end_time = time(23, 59)  
+        current_time = datetime.now().time()  # Текущее времени
+        start_time = time(9, 0)  # Задание начала рабочего дня
+        end_time = time(23, 59)  # Задание конца рабочего дня
 
-        if not (start_time <= current_time <= end_time):
-            return Response({'error': 'Печать талонов недоступна в данный момент'}, status=403)
-        
+        if not (start_time <= current_time <= end_time):  # Проверка на рабочее время
+            return Response({'error': 'Печать талонов недоступна в данный момент'}, status=403)  # Возврат ошибки 403
 
         try:
-            customer = Customer.objects.get(ticket_number=pk)
+            customer = Customer.objects.get(ticket_number=pk)  # Получение объекта талона по номеру 
         except Customer.DoesNotExist:
-            return Response({'error': 'Талон с указанным номером не найден'}, status=404)
-        
+            return Response({'error': 'Талон с указанным номером не найден'}, status=404)  # Возврат ошибки 404, если талон с указанным номером не найден
 
         def calculate_estimated_wait_time(queue, customer):
-            ahead_customers = Customer.objects.filter(queue=queue, is_served=False, ticket_number__lt=customer.ticket_number)
-            average_waiting_time = queue.average_waiting_time or 0
+            ahead_customers = Customer.objects.filter(queue=queue, is_served=False, ticket_number__lt=customer.ticket_number)  # Получение всех талонов, находящихся в очереди перед указанным талоном
+            average_waiting_time = queue.average_waiting_time or 0  # Получение среднего времени ожидания для очереди или 0, если значение отсутствует
 
-            total_waiting_time = ahead_customers.count() * average_waiting_time
-            estimated_wait_time = datetime.now() + timedelta(minutes=total_waiting_time)
-            time_remaining = estimated_wait_time - datetime.now()
+            total_waiting_time = ahead_customers.count() * average_waiting_time  # Вычисление общего времени ожидания для всех талонов перед указанным талоном
+            estimated_wait_time = datetime.now() + timedelta(minutes=total_waiting_time)  # Вычисление примерного времени ожидания для указанного талона
+            time_remaining = estimated_wait_time - datetime.now()  # Вычисление оставшегося времени ожидания
 
-            minutes = time_remaining.seconds // 60
-            seconds = time_remaining.seconds % 60
+            minutes = time_remaining.seconds // 60  
+            seconds = time_remaining.seconds % 60  
 
-            time_remaining_str = f"{minutes} минут, {seconds} секунд"
+            time_remaining_str = f"{minutes} минут, {seconds} секунд"  
 
             return time_remaining_str
 
-
-        
         ticket_data = {
-            'Номер талона': customer.ticket_number,
-            'Выдано': customer.created_at,
-            'Имя посетителя': customer.user.username,
-            'Наименование организации': 'RSK',
-            'Очередь': customer.queue.name,
-            'Номер окна': customer.queue.window_number,
-            'Количество посетителей в очереди': Customer.objects.filter(queue=customer.queue, is_served=False, ticket_number__lt=customer.ticket_number).count(),
-            'Примерное время ожидания': calculate_estimated_wait_time(customer.queue, customer),
-            }
-        return Response(ticket_data)
+            'Номер талона': customer.ticket_number, 
+            'Выдано': customer.created_at,  
+            'Имя посетителя': customer.user.username, 
+            'Наименование организации': 'RSK',  
+            'Очередь': customer.queue.name, 
+            'Номер окна': customer.queue.window_number, 
+            'Количество посетителей в очереди': Customer.objects.filter(queue=customer.queue, is_served=False, ticket_number__lt=customer.ticket_number).count(),  
+            'Примерное время ожидания': calculate_estimated_wait_time(customer.queue, customer), 
+        }
+        return Response(ticket_data)  
+
 
 
