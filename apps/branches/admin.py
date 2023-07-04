07a6e27f03f05581902a5631e95b5cd595ac894d
SANJIKS <sanzarmaratov588@gmail.com>
2023-07-04 14:30:59 +0600

    added models to admin

--- apps/branches/admin.py
+++ apps/branches/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Branch, Terminal, Window, Region, Calendar
+from .models import Branch, Terminal, Window, Region, Calendar, BaseCalendar, Floor, Cabinet
 
-admin.site.register([Branch, Terminal, Window, Region, Calendar])
+admin.site.register([Branch, Terminal, Window, Region, Calendar, BaseCalendar, Floor, Cabinet])
