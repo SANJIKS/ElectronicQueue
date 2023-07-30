3df9f1aa8e7214255c9873d78f2355e1d920d56b
SANJIKS <sanzaramratov588@gmail.com>
2023-07-30 17:24:32 +0600

    added comments for chat endpoints

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,14 +1,20 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import (ChatGroupSerializer, MessageSerializer, 
                           PrivateChatSerializer, PrivateMessageSerializer)
 from .models import Message, PrivateMessage 
-
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 class ChatGroupView(generics.CreateAPIView):
     serializer_class = ChatGroupSerializer
+
+    @swagger_auto_schema(operation_description="Создание группового чата", operation_summary="Создание группового чата")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 class MessageView(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
@@ -18,6 +24,11 @@ class MessageView(generics.ListCreateAPIView):
         chat_group_id = self.kwargs.get('chat_group_id')
         return Message.objects.filter(chat_group_id=chat_group_id)
 
+    @swagger_auto_schema(operation_description="Получение всех сообщений в групповом чате", operation_summary="Получение сообщений")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Отправка сообщения в групповой чат", operation_summary="Отправка сообщения")
     def perform_create(self, serializer):
         chat_group_id = self.kwargs.get('chat_group_id')
         message = serializer.save(sender=self.request.user, chat_group_id=chat_group_id)
@@ -34,6 +45,11 @@ class MessageView(generics.ListCreateAPIView):
 class PrivateChatView(generics.CreateAPIView):
     serializer_class = PrivateChatSerializer
 
+    @swagger_auto_schema(operation_description="Создание приватного чата", operation_summary="Создание приватного чата")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+
 class PrivateMessageView(generics.ListCreateAPIView):
     serializer_class = PrivateMessageSerializer
     permission_classes = [IsAuthenticated]
@@ -42,6 +58,11 @@ class PrivateMessageView(generics.ListCreateAPIView):
         private_chat_id = self.kwargs.get('private_chat_id')
         return PrivateMessage.objects.filter(private_chat_id=private_chat_id)
 
+    @swagger_auto_schema(operation_description="Получение всех сообщений в приватном чате", operation_summary="Получение сообщений")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Отправка сообщения в приватный чат", operation_summary="Отправка сообщения")
     def perform_create(self, serializer):
         private_chat_id = self.kwargs.get('private_chat_id')
         private_message = serializer.save(sender=self.request.user, private_chat_id=private_chat_id)
