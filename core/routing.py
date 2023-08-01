0fd3919baa39f2afd1d40a5a400beb6af0d73657
SANJIKS <sanzaramratov588@gmail.com>
2023-08-01 17:45:41 +0600

    fix

--- core/routing.py
+++ core/routing.py
@@ -5,6 +5,7 @@ from apps.chat.consumers import PrivateChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/customers/(?P<userId>\w+)/$', TicketConsumer.as_asgi()),
+    re_path(r'ws/waitlist/(?P<branch_id>\w+)/$', CabinetConsumer.as_asgi()),
     re_path(r'ws/board/(?P<branch_id>\w+)/$', ScoreBoardConsumer.as_asgi()),
     re_path(r'ws/private_chat/(?P<user_id_1>\d+)/(?P<user_id_2>\d+)/$', PrivateChatConsumer.as_asgi()),
 ]
